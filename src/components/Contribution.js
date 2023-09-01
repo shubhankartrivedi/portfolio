c6f3e2a477c7a7adbb99439ad5a98951d61b5700
Shubhankar <shubhankartrivedi1234@gmail.com>
2023-09-01 21:45:16 +0530

    v0.1

---
+++ src/components/Contribution.js
@@ -0,0 +1,15 @@
+"use client"
+
+import GitHubCalendar from 'react-github-calendar';
+
+
+
+export default function contribution() {
+
+    return (
+        <div className='animate-fade-in'>
+<GitHubCalendar username="shubhankartrivedi" />
+</div>
+)
+
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,38 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      animation: {
+        'scale-up': 'scaleUp 1s ease-in 1',
+        'fade-in': 'fadeIn 2s ease-in-out 1',
+      },
+      keyframes: () => ({
+        scaleUp: {
+          '0%': { transform: 'scale(100%)' },
+          '40%': { transform: 'scale(70%)', filter: 'blur(1px)' },
+          '100%': { transform: 'scale(1000%)', filter: 'blur(100px)' },
+        },
+        fadeIn: {
+          '0%': { opacity: 0 },
+          '50%': { opacity: 0},
+          '100%': { opacity: 100},
+        }
+      }),
+      fontSize: {
+        xs: ['0.75rem', { lineHeight: '1.5' }],
+        sm: ['0.875rem', { lineHeight: '1.5715' }],
+        base: ['1rem', { lineHeight: '1.5', letterSpacing: '-0.01em' }],
+        lg: ['1.125rem', { lineHeight: '1.5', letterSpacing: '-0.01em' }],
+        xl: ['1.25rem', { lineHeight: '1.5', letterSpacing: '-0.01em' }],
+        '2xl': ['1.5rem', { lineHeight: '1.415', letterSpacing: '-0.01em' }],
+        '3xl': ['1.875rem', { lineHeight: '1.333', letterSpacing: '-0.01em' }],
+        '4xl': ['2.25rem', { lineHeight: '1.277', letterSpacing: '-0.01em' }],
+        '5xl': ['3rem', { lineHeight: '1.2', letterSpacing: '-0.01em' }],
+        '6xl': ['3.75rem', { lineHeight: '1.166', letterSpacing: '-0.01em' }],
+        '7xl': ['5rem', { lineHeight: '1', letterSpacing: '-0.01em' }],
+      },
+
     },
+    
   },
   plugins: [],
 }
